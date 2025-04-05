9f54890c5d8ec888ade846591d6de4b262cf8fc6
benaleo <benaleobayu@gmail.com>
2025-04-05 08:33:52 +0700

    add shadcn ui

--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -1,14 +1,27 @@
 <script setup>
+import Button from '@/components/ui/button/Button.vue';
 import { ref } from 'vue';
 
 const feature = ref();
+
+const login = () => {
+  window.location.href = '/login';
+};
+
+const register = () => {
+  window.location.href = '/register';
+};
 </script>
 
 <template>
   <div class="w-full h-screen flex justify-center items-center bg-style">
-    <div class="flex flex-col items-center">
+    <div class="flex flex-col items-center gap-4">
       <h1 class="text-white">Hallo</h1>
       <h3 class="text-white">Selamat datang di Sistem Back Office Kasir Pinter</h3>
+      <div class="flex gap-2">
+        <Button variant="add" @click="login">Masuk</Button>
+        <Button variant="green" @click="register">Daftar</Button>
+      </div>
     </div>
   </div>
 </template>
@@ -17,5 +30,7 @@ const feature = ref();
 .bg-style {
   background-image: url('../assets/default/left_side_login.png');
   background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
 }
 </style>
